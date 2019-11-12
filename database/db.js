fc71b2257ac1fdbbc43bcda78a07c2b9cf953936
sjovanovic974 <sjovanovic974@gmail.com>
2019-11-12 23:14:32 +0100

    Connection string added

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const knex = require("knex");
 module.exports = knex({
   client: "pg",
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     //user: process.env.DB_USER,
     //password: process.env.DB_PASSWORD,
     //database: process.env.DB_NAME,
